6d94dac3906200b53a0e9e92277ff7751729861d
Manoj Kumar <manoj.kumar.mbm@gmail.com>
2016-04-03 13:11:57 +0530

    Genric complete

---
+++ genricClassMultipleDataTypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class k, class v>
+
+class MyMap{
+	private:
+		std::vector<k> keys;
+		std::vector<v> values;
+	public:
+		void insert(k key,v value ){
+			keys.push_back(key);
+			values.push_back(value);
+		}	
+
+		void getValue(int n){
+			std::cout<<keys[n]<<" <-- key"<<" value-->"<<values[n];
+		}
+};
+
+int main(){
+	MyMap<int,std::string> map;
+	map.insert(1,"manoj");
+	map.getValue(0);
+}

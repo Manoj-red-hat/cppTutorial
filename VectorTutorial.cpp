52964446b46c153e52ba8344bd050f24d892641d
Manoj Kumar <manoj.kumar.mbm@gmail.com>
2016-04-11 21:48:42 +0530

    vector end

--- VectorTutorial.cpp
+++ VectorTutorial.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 
 using namespace std;
 
 void print(vector<int> &ref){
 	vector<int>::iterator itr;
-	for(itr=ref.begin();itr<ref.end();itr++){cout<<*itr<<endl;}
+	/*Using ostream iterator */	
+	copy(ref.begin(),ref.end(),ostream_iterator<int>(cout,","));
+	cout<< "  size is "<<ref.size()<<\n";
+
+	/*Using for loop*/
+	//for(itr=ref.begin();itr<ref.end();itr++){cout<<*itr<<endl;}
 }
 int main(){
 	vector<int> number;
@@ -15,6 +21,12 @@ int main(){
 	number.push_back(4);
 	number.push_back(5);
 	number.push_back(6);
-
 	print(number);
+	number.insert(number.begin()+5,12); //insert a element and expand
+	print(number);
+	number.erase(number.begin()+2); //erase a element and squueze vector
+	print(number);
+	number.push_back(34); //Push a element at end
+	print(number);
+	
 }
